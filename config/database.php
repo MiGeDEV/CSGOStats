fa942eff67dc6d4903b23323b6d60281a534ff8c
xBloodshed <millir@migedev.com>
2015-12-19 13:43:49 +0530

    database conn

---
+++ config/database.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Milli
+ * Date: 19-12-2015
+ * Time: 01:36 PM
+ */
+
+$db_host = "localhost";     //database server. Normally, localhost is sufficient if on the same machine
+
+$db_name = "database";      //database to use
+
+$db_username = "username";  //database username
+
+$db_password = "password";  //database password
+
+$db_table = "rankme";       //database table being used
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Milli
+ * Date: 19-12-2015
+ * Time: 01:36 PM
+ */
